c18eb423fefdb35f2adb6a2b49d6378d22152a03
liuwei <liuweizhenhaoa@163.com>
2019-06-18 17:03:21 +0800

    add  grpc demo

---
+++ summer-grpc/src/main/java/com/jintdev/bus/smartboard/SmartBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jintdev.bus.smartboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBoardApplication.class, args);
+    }
+
+}
---
+++ summer-grpc/src/main/java/com/jintdev/bus/smartboard/grpc/GreeterService1.java
@@ -0,0 +1,6 @@
+package com.jintdev.bus.smartboard.grpc;
+
+import com.jintdev.bus.proto.GreeterGrpc;
+
+public class GreeterService1 extends GreeterGrpc.GreeterImplBase{
+}
