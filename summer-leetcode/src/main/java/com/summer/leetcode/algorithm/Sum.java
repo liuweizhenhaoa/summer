0b72631feef511cab28e06591af65300b7d346ba
liuwei <liuweizhenhaoa@163.com>
2019-07-08 09:25:57 +0800

    新增leetcode

---
+++ summer-leetcode/src/main/java/com/summer/leetcode/algorithm/Sum.java
@@ -0,0 +1,64 @@
+package com.summer.leetcode.algorithm;
+
+import java.util.HashMap;
+
+public class Sum {
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *
+     * 示例:
+     *
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     *
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]+nums[j]==target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                return new int[]{map.get(target-nums[i]),i};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSum3(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target-nums[i])) {
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+
+}
