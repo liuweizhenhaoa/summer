d7ad780420ffd092237b41df134214c5426f1739
wei liu <liuweizhenhaoa@163.com>
2019-08-23 14:18:46 +0800

    新增valid。及	webflux

---
+++ summer-springboot/summer-springboot-valid/src/main/java/com/summer/springboot/valid/ValidApplication.java
@@ -0,0 +1,13 @@
+package com.summer.springboot.valid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidApplication.class, args);
+    }
+
+}
---
+++ summer-springboot/summer-springboot-valid/src/main/java/com/summer/springboot/valid/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.summer.springboot.valid.controller;
+
+import com.summer.springboot.valid.entity.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/valid")
+@Slf4j
+public class OrderController {
+
+
+    @GetMapping("/test")
+    public Map queryOrder(@Valid Order order, BindingResult result){
+        if(result.hasErrors()){
+            for (ObjectError error : result.getAllErrors()) {
+                log.error(error.getDefaultMessage());
+            }
+        }
+        log.info("--------------"+order.toString());
+        return null;
+    }
+
+    @GetMapping("/test1")
+    public Map queryOrder1(@Valid Order order){
+
+        log.info("--------------"+order.toString());
+        return null;
+    }
+
+
+    @GetMapping("/testVal")
+    public void testVal(@PathVariable(value="pageNum")int pageNum, @PathVariable(value="pageSize")int pageSize){
+
+
+    }
+
+}
